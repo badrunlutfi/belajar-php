47371118974a125409e156353627313cc76f3f2e
Muhamad Badrun Lutfi <badrunlutfi879@gmail.com>
2022-11-24 17:34:08 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+echo "Hallo!<br>";
+
+$nama = "Muhamad badrun lutfi";
+
+$umur = 21;
+
+echo "Nama saya $nama, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Badrun";
+$umurAdik = "15";
+
+echo "Nama Adik Saya <strong>$namaAdik</strong>, adik saya berusia
+$umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihumur tahun." 
+
+?>
